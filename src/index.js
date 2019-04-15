d307e496185449c83c8849e21852a538e2280c5d
Bernardo Pinheiro <bernardoppinheiro@gmail.com>
2019-04-15 15:08:45 +0100

    Season app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 
 //teste
@@ -30,7 +31,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>
+        return <Spinner />
     }
 }
 ReactDOM.render( <App /> , document.querySelector('#root'));
